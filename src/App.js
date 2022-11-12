4534e7fa16d6a91a67fb88185d198f874aa0bd1f
Wael Mashal <wael.mashal@sap.com>
2022-11-12 13:24:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector,useDispatch } from 'react-redux';
 
 function App() {
+  const dispatcher = useDispatch();
+  const increment = () => {
+    dispatcher({type:"INC"})
+  }
+
+  const decrement = () => {
+    dispatcher({type:"DEC"})
+  }
+
+  const addByTen = () => {
+    dispatcher({type:"addByTen",payload:10})
+  }
+
+  const counter = useSelector((state) => state.counter)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={addByTen}>Add 10</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import {createStore} from 'redux'
+
+const reducerFn = (state={counter:0},action) => {
+    if(action.type === "INC"){
+       return  {counter:state.counter + 1};
+    }
+    if(action.type === "DEC"){
+        return  {counter:state.counter - 1};
+     }
+
+     if(action.type === "addByTen"){
+        return  {counter:state.counter + action.payload};
+     }
+    return state;
+}
+
+const store = createStore(reducerFn)
+export default store;
